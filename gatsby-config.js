3e52075a43028ee07649364b706ed3368a585433
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-08-01 13:48:24 -0400

    fix rss url and guid links

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,8 +92,8 @@ module.exports = {
               ...edge.node.frontmatter,
               description: edge.node.excerpt,
               date: edge.node.frontmatter.date,
-              url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-              guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+              url: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
+              guid: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
               custom_elements: [{ 'content:encoded': edge.node.html }],
             })),
             query: `
