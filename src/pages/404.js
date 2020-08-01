e9c212d97145efa813041d270fb4bcc0705d78f9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-07-31 22:08:37 -0400

    add 404 text styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,14 @@ import Layout from '../components/Layout';
 export default function Error() {
   return (
     <Layout>
-      <h1>Oops the route doesn&apos;t exist</h1>
-      <Link to='/'>
-        Back to home
-        {' '}
-        <span>&rarr;</span>
-      </Link>
+      <div className='text-center'>
+        <h1 className='font-heading text-4xl text-primary text-center my-4'>Oops the route doesn&apos;t exist</h1>
+        <Link to='/' className='text-primary'>
+          Back to home
+          {' '}
+          <span>&rarr;</span>
+        </Link>
+      </div>
     </Layout>
-  )
+  );
 }
