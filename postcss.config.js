94b6e8a404bbd5c91357831b007aa2d1fb637431
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-12-03 18:24:55 -0500

    configure purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
@@ -7,5 +8,10 @@ module.exports = {
   plugins: [
     require('tailwindcss')('tailwind.config.js'),
     require('autoprefixer')(),
+    process.env.NODE_ENV === 'production' &&
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./src/**/*.js', './public/**/*.html'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
   ],
 };
