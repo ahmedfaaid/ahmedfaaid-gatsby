477fcf190846ac11c925c07f864734a9005e3589
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-07-25 18:16:55 -0400

    tailwind and mdx config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-  plugins: [
-    require('tailwindcss')('tailwind.config.js'),
-    require('autoprefixer')(),
-    process.env.NODE_ENV === 'production' &&
-      require('@fullhuman/postcss-purgecss')({
-        content: ['./src/**/*.js', './public/**/*.html'],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        safelist: ['pre'],
-      }),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
