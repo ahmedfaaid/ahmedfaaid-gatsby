ac502c7d79e26ab5092e9c82da7962e72b99444e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-07-25 18:51:31 -0400

    blog page & post card image

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Img from 'gatsby-image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 
-export default function Post({
-  title,
-  description,
-  slug,
-  fluid,
-}) {
+export default function Post({ title, description, slug, imageData }) {
+  const image = getImage(imageData);
   return (
     <div className='bg-dark-3 w-4/5 m-auto rounded-sm shadow-md mb-12 p-3'>
       <Link to={`/${slug}`}>
-        <Img
-          className='w-full m-auto mb-8'
-          fluid={fluid}
-          alt='Blog Image'
-        />
+        <GatsbyImage image={image} alt={title} />
       </Link>
       <Link to={`/${slug}`}>
         <h3 className='font-heading text-xl text-white my-5'>{title}</h3>
@@ -34,5 +26,5 @@ Post.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   slug: PropTypes.string.isRequired,
-  fluid: PropTypes.instanceOf(Object).isRequired,
+  imageData: PropTypes.instanceOf(Object).isRequired,
 };
