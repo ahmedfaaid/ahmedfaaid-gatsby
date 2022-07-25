a9b9e44d892e2aa4d35a085d68c40fc05fc37ad8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-07-25 18:50:19 -0400

    gatsby image config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
